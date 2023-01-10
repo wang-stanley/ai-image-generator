1a77f2d6e5dd9e4f2a65de63575950c5634837a3
wang-stanley <wang4928@purdue.edu>
2023-01-10 13:52:54 -0500

    Basic functionality completed. Copied css from Fireship repo

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener('submit', async (e) => {
+  e.preventDefault(); // Prevents default behavior which refreshes the entire page
+
+  const data = new FormData(form);
+
+  const response = await fetch('http://localhost:8080/dream', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  const { image } = await response.json();
+
+  const result = document.querySelector('#result');
+  result.innerHTML = `<img src="${image}" width="512" />`;
+});
